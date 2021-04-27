eb781471b1f305f6fc1021ae2c44b374060bef30
lloopy123123543 <lloopy123123543@gmail.com>
2021-04-27 17:04:27 +0300

    ez

---
+++ main.js
@@ -0,0 +1,24 @@
+let game = new Game();
+
+game.initMap();
+
+let cube = {
+    id: 'cube',
+    x: 0,
+    y: 0,
+    width: 56,
+    height: 56,
+    trnstn: .25,
+    step: 56,
+    color: 'black'
+}
+
+game.pushBot();
+game.pushBuff();
+
+renderMap(game.getMap());
+renderCube(spawnCube(game.getMap(), cube));
+
+
+
+let tike = setInterval (buffsLogic, 1000);
